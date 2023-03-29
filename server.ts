c32301f4450c56c328446e2f414b1109e7910447
pablo <pablo_vitor1707@hotmail.com>
2023-03-29 07:41:35 -0300

    Testando Api pelo Insomnia 2

--- server.ts
+++ server.ts
@@ -4,8 +4,7 @@ import jwt from 'jsonwebtoken';
 import { PrismaClient } from '@prisma/client';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
-import bodyParser from 'body-parser';
-import routes from './routes/routes'
+import routes from './routes/routes';
 
 const prisma = new PrismaClient();
 const app = express();
@@ -13,31 +12,9 @@ const port = 3000;
 
 // middlewares
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // rotas
-app.post('/users', async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password,
-    },
-  });
-  res.json(user);
-});
-
-app.get('/users', async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
-
-
-
 app.use(routes);
-
 
 // Swagger documentation setup
 const swaggerOptions = {
