8623372061e972afca16665716b5821e0cedbd61
pablo <pablo_vitor1707@hotmail.com>
2023-04-04 22:42:34 -0300

    Limpando os compiladores

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -4,26 +4,35 @@ import { Request, Response } from 'express';
 const prisma = new PrismaClient();
 
 export default class UserController {
+  static default: any;
   static async get(req: Request, res: Response) {
     const { id } = req.params;
-    const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
-    if (user) {
-      return res.status(200).json(user);
-    }
-    return res.status(404).json({ mensagem: 'Usuário não encontrado' });
-  }
 
-  static async post(req: Request, res: Response) {
-    const { name, email, password } = req.body;
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-        email,
-        password
+    // Verifica se o ID foi fornecido na requisição
+    if (id) {
+      const user = await prisma.user.findUnique({ where: { id: parseInt(id) } });
+      if (user) {
+        return res.status(200).json(user);
       }
-    });
-    return res.status(201).json({ mensagem: 'Usuário criado com sucesso', id: newUser.id });
-  }
+      return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+    } else {
+      // Retorna todos os usuários se o ID não foi fornecido
+      const users = await prisma.user.findMany();
+      return res.status(200).json(users);
+    }
+}
+
+static async post(req: Request, res: Response) {
+  const { name, email, password } = req.body;
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password
+    }
+  });
+  return res.status(201).json({ mensagem: 'Usuário criado com sucesso', id: newUser.id });
+}
 
   static async put(req: Request, res: Response) {
     const { id } = req.params;
