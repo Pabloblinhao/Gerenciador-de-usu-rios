cf403ef9905018c8ea4dd741523e2bd4ae8832a6
pablo <pablo_vitor1707@hotmail.com>
2023-04-04 22:42:14 -0300

    Limpando os compiladores

--- routes/routes.ts
+++ routes/routes.ts
@@ -3,9 +3,10 @@ import UserController from '../controllers/UserController';
 
 const router = Router();
 
-router.get('/users/:id', UserController.get);
+router.get('/users/:id?', UserController.get);
 router.post('/users', UserController.post);
 router.put('/users/:id', UserController.put);
 router.delete('/users/:id', UserController.delete);
 
 export default router;
+
